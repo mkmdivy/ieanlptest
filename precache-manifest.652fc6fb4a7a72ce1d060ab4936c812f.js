58c19e109f5bcd8920b06071b3f54d61d8dd776d
Inhoi Heo <inhoi.heo@oecd.org>
2021-12-05 00:40:34 +0100

    Updates

---
+++ precache-manifest.652fc6fb4a7a72ce1d060ab4936c812f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09650fd3be6200c7398100617e2ec0e3",
+    "url": "/ieanlptest/index.html"
+  },
+  {
+    "revision": "fd2d913c504034e16819",
+    "url": "/ieanlptest/static/css/main.a5578d3d.chunk.css"
+  },
+  {
+    "revision": "d97aadac82c778645278",
+    "url": "/ieanlptest/static/js/2.d5e08e07.chunk.js"
+  },
+  {
+    "revision": "1fbd3b3b57187c830d43cf1b0fb1d8cb",
+    "url": "/ieanlptest/static/js/2.d5e08e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2d913c504034e16819",
+    "url": "/ieanlptest/static/js/main.3c59adff.chunk.js"
+  },
+  {
+    "revision": "f239d28af9fe9a1e79d4",
+    "url": "/ieanlptest/static/js/runtime-main.58d8a6c0.js"
+  }
+]);
